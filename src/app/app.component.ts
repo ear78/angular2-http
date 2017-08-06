c8170958206f103e36554bda3b845c4cc9e64652
elliot richardson <ear78@aol.com>
2017-08-05 22:27:37 -0500

    added simple get setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpModule, Http, URLSearchParams, Headers, RequestOptions} from '@angular/http';
+
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    apiRoot: string = "http://httpbin.org";
+
+    constructor(private http: Http){}
+
+    doGET(){
+        console.log('GET');
+        let url = `${this.apiRoot}/get`;
+        this.http.get(url).subscribe(res => console.log(res.json()));
+    }
+
+  //   doPOST() {
+  //   console.log("POST");
+  //   let url = `${this.apiRoot}/post`;
+  //   let search = new URLSearchParams();
+  //   search.set('foo', 'moo');
+  //   search.set('limit', 25);
+  //   this.http.post(url, {moo: 'foo', goo: 'loo'}, {search}).subscribe(res => console.log(res.json()));
+  // }
+  //
+  // doPUT() {
+  //   console.log("PUT");
+  //   let url = `${this.apiRoot}/put`;
+  //   let search = new URLSearchParams();
+  //   search.set('foo', 'moo');
+  //   search.set('limit', 25);
+  //   this.http.put(url, {moo: "foo", goo: "loo"}, {search}).subscribe(res => console.log(res.json()));
+  // }
+  //
+  // doDELETE() {
+  //   console.log("DELETE");
+  //   let url = `${this.apiRoot}/delete`;
+  //   let search = new URLSearchParams();
+  //   search.set('foo', 'moo');
+  //   search.set('limit', 25);
+  //   this.http.delete(url, {search}).subscribe(res => console.log(res.json()));
+  // }
+  //
+  // doGETAsPromiseError() {
+  //   console.log("GET AS PROMISE ERROR");
+  //   let url = `${this.apiRoot}/post`;
+  //   this.http.get(url)
+  //       .toPromise()
+  //       .then(
+  //           res => console.log(res.json()),
+  //           msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+  //       );
+  // }
+  //
+  // doGETAsObservableError() {
+  //   console.log("GET AS OBSERVABLE ERROR");
+  //   let url = `${this.apiRoot}/post`;
+  //   this.http.get(url).subscribe(
+  //       res => console.log(res.json()),
+  //       msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+  //   );
+  // }
+  //
+  // doGETWithHeaders() {
+  //   console.log("GET WITH HEADERS");
+  //   let headers: Headers = new Headers();
+  //   headers.append('Authorization', btoa('username:password'));
+  //   let opts: RequestOptions = new RequestOptions();
+  //   opts.headers = headers;
+  //   let url = `${this.apiRoot}/get`;
+  //   this.http.get(url, opts).subscribe(
+  //       res => console.log(res.json()),
+  //       msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+  //   );
+  // }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http, URLSearchParams, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/Rx';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
