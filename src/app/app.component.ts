ce7246d4b20496e4ef1cb7cc3379edd406cc4b41
elliot richardson <ear78@aol.com>
2017-08-05 22:55:34 -0500

    finished test app with promise examples and subscriber examples, also added error handling with catch method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpModule, Http, URLSearchParams, Headers, RequestOptions} from '@angular/http';
 
-import 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -17,68 +17,79 @@ export class AppComponent {
     doGET(){
         console.log('GET');
         let url = `${this.apiRoot}/get`;
-        this.http.get(url).subscribe(res => console.log(res.json()));
+        let search = new URLSearchParams();
+        search.set('foo', 'moo');
+        search.set('limit', '25');
+        this.http.get(url, {search: search}).subscribe(res => console.log(res.json()));
     }
 
-  //   doPOST() {
-  //   console.log("POST");
-  //   let url = `${this.apiRoot}/post`;
-  //   let search = new URLSearchParams();
-  //   search.set('foo', 'moo');
-  //   search.set('limit', 25);
-  //   this.http.post(url, {moo: 'foo', goo: 'loo'}, {search}).subscribe(res => console.log(res.json()));
-  // }
-  //
-  // doPUT() {
-  //   console.log("PUT");
-  //   let url = `${this.apiRoot}/put`;
-  //   let search = new URLSearchParams();
-  //   search.set('foo', 'moo');
-  //   search.set('limit', 25);
-  //   this.http.put(url, {moo: "foo", goo: "loo"}, {search}).subscribe(res => console.log(res.json()));
-  // }
-  //
-  // doDELETE() {
-  //   console.log("DELETE");
-  //   let url = `${this.apiRoot}/delete`;
-  //   let search = new URLSearchParams();
-  //   search.set('foo', 'moo');
-  //   search.set('limit', 25);
-  //   this.http.delete(url, {search}).subscribe(res => console.log(res.json()));
-  // }
-  //
-  // doGETAsPromiseError() {
-  //   console.log("GET AS PROMISE ERROR");
-  //   let url = `${this.apiRoot}/post`;
-  //   this.http.get(url)
-  //       .toPromise()
-  //       .then(
-  //           res => console.log(res.json()),
-  //           msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
-  //       );
-  // }
-  //
-  // doGETAsObservableError() {
-  //   console.log("GET AS OBSERVABLE ERROR");
-  //   let url = `${this.apiRoot}/post`;
-  //   this.http.get(url).subscribe(
-  //       res => console.log(res.json()),
-  //       msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
-  //   );
-  // }
-  //
-  // doGETWithHeaders() {
-  //   console.log("GET WITH HEADERS");
-  //   let headers: Headers = new Headers();
-  //   headers.append('Authorization', btoa('username:password'));
-  //   let opts: RequestOptions = new RequestOptions();
-  //   opts.headers = headers;
-  //   let url = `${this.apiRoot}/get`;
-  //   this.http.get(url, opts).subscribe(
-  //       res => console.log(res.json()),
-  //       msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
-  //   );
-  // }
+    doPOST() {
+    console.log("POST");
+    let url = `${this.apiRoot}/post`;
+    let search = new URLSearchParams();
+    search.set('foo', 'moo');
+    search.set('limit', '25');
+    this.http.post(url, {moo: 'foo', goo: 'loo'}, {search}).subscribe(res => console.log(res.json()));
+  }
+
+  doPUT() {
+    console.log("PUT");
+    let url = `${this.apiRoot}/put`;
+    let search = new URLSearchParams();
+    search.set('foo', 'moo');
+    search.set('limit', '25');
+    this.http.put(url, {moo: "foo", goo: "loo"}, {search}).subscribe(res => console.log(res.json()));
+  }
+
+  doDELETE() {
+    console.log("DELETE");
+    let url = `${this.apiRoot}/delete`;
+    let search = new URLSearchParams();
+    search.set('foo', 'moo');
+    search.set('limit', '25');
+    this.http.delete(url, {search}).subscribe(res => console.log(res.json()));
+  }
+
+  doGETAsPromise() {
+    console.log("GET AS PROMISE");
+    let url = `${this.apiRoot}/get`;
+    this.http.get(url)
+        .toPromise()
+        .then(res => console.log(res.json()));
+  }
+
+  doGETAsPromiseError() {
+    console.log("GET AS PROMISE ERROR");
+    let url = `${this.apiRoot}/post`;
+    this.http.get(url)
+        .toPromise()
+        .then(
+            res => console.log(res.json()),
+            // YOU CAN USE THE CATCH METHOD AS WELL TO BE MORE EXPLICIT
+        ).catch(msg => console.error(`Error: ${msg.status} ${msg.statusText}`));
+  }
+
+  doGETAsObservableError() {
+    console.log("GET AS OBSERVABLE ERROR");
+    let url = `${this.apiRoot}/post`;
+    this.http.get(url).subscribe(
+        res => console.log(res.json()),
+        msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+    );
+  }
+
+  doGETWithHeaders() {
+    console.log("GET WITH HEADERS");
+    let headers: Headers = new Headers();
+    headers.append('Authorization', btoa('username:password'));
+    let opts: RequestOptions = new RequestOptions();
+    opts.headers = headers;
+    let url = `${this.apiRoot}/get`;
+    this.http.get(url, opts).subscribe(
+        res => console.log(res.json()),
+        msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+    );
+  }
 
 
 }
